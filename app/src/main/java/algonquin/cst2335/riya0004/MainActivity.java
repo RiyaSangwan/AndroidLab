38251d149de5edf00bc4be0c848e406509898595
Riya Sangwan <riya0004@algonquinlive.com>
2023-06-01 16:38:20 -0400

    Finished Lab3_Step3

--- app/src/main/java/algonquin/cst2335/riya0004/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/riya0004/MainActivity.java
@@ -3,6 +3,11 @@ package algonquin.cst2335.riya0004;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View imgView = findViewById(R.id.flagview);
+        Switch sw = findViewById(R.id.switch1);
+
+        sw.setOnCheckedChangeListener((btn, isChecked)->{
+
+            if(isChecked){
+                RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+                rotate.setDuration(5000);
+                rotate.setRepeatCount(Animation.INFINITE);
+                rotate.setInterpolator(new LinearInterpolator());
+
+                imgView.startAnimation(rotate);
+            }
+            else {
+                imgView.clearAnimation();
+            }
+        });
+
     }
 }
